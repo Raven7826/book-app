327ce9561bb276fd0ca9b99334eb696102627ab5
Michael Ferguson <mike.ferguson2678@icloud.com>
2025-03-18 17:55:22 -0400

    Javascript

--- app.js
+++ app.js
@@ -51,6 +51,18 @@ class UI {
         }
     }
 
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        // Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -74,16 +86,26 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    // Instatiate book
-    const book = new Book(title, author, isbn);
+    // Validate
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+        // Instatiate book
+        const book = new Book(title, author, isbn);
 
-    //Add a Book to UI
-    UI.addBookToList(book);
+        //Add a Book to UI
+        UI.addBookToList(book);
 
-    // Clear fields
-    UI.clearFields(title, author, isbn);
+        // Show success message
+        UI.showAlert('Book Added', 'success');
 
+        // Clear fields
+        UI.clearFields(title, author, isbn);
+
+    }
     console.log('HTML');
+
+
 
 });
 
@@ -92,6 +114,8 @@ document.querySelector('#book-list').addEventListener('click', (e) => {
 
     UI.deleteBook(e.target)
 
+    // Show success message
+    UI.showAlert('Book Removed', 'success');
 });
 
 
